cf8977a95a40eee4e4c50ba73da722ee56815e4d
Matt Birum <45514707+mbirum@users.noreply.github.com>
2020-10-05 21:13:42 -0400

    Update motor.py

--- motor.py
+++ motor.py
@@ -5,41 +5,39 @@ control_pins = [7,11,13,15]
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
-  
-forward = [
-  [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
-  [0,1,1,0],
-  [0,0,1,0],
-  [0,0,1,1],
-  [0,0,0,1],
-  [1,0,0,1]
-]
-
-reverse = [
-  [1,0,0,1],
-  [0,0,0,1],
-  [0,0,1,1],
-  [0,0,1,0],
-  [0,1,1,0],
-  [0,1,0,0],
-  [1,1,0,0],
-  [1,0,0,0]
-]
 
 def forward():
+  halfsteps = [
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1],
+    [1,0,0,1]
+  ]
   for i in range(100):
     for halfstep in range(8):
       for pin in range(4):
-        GPIO.output(control_pins[pin], forward[halfstep][pin])
+        GPIO.output(control_pins[pin], halfsteps[halfstep][pin])
       time.sleep(0.001)
 
 def reverse():
+  halfsteps = [
+    [1,0,0,1],
+    [0,0,0,1],
+    [0,0,1,1],
+    [0,0,1,0],
+    [0,1,1,0],
+    [0,1,0,0],
+    [1,1,0,0],
+    [1,0,0,0]
+  ]
   for i in range(100):
     for halfstep in range(8):
       for pin in range(4):
-        GPIO.output(control_pins[pin], reverse[halfstep][pin])
+        GPIO.output(control_pins[pin], halfsteps[halfstep][pin])
       time.sleep(0.001)
 
 forward()
