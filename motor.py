92213e5288008eb88e653c1537ec3b64a5328123
Matt Birum <45514707+mbirum@users.noreply.github.com>
2020-10-05 20:54:42 -0400

    Update motor.py

--- motor.py
+++ motor.py
@@ -16,10 +16,10 @@ halfstep_seq = [
   [1,0,0,1]
 ]
 
-#for i in range(512):
-for halfstep in range(8):
-  for pin in range(4):
-    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-  time.sleep(0.001)
+for i in range(512):
+  for halfstep in range(8):
+    for pin in range(4):
+      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+    time.sleep(0.001)
 
 GPIO.cleanup()
