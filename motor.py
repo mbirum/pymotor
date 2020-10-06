f689e3bb6aea3ccfe25daa1da6130ce237fb31e8
Matt Birum <45514707+mbirum@users.noreply.github.com>
2020-10-05 21:08:25 -0400

    Update motor.py

--- motor.py
+++ motor.py
@@ -5,7 +5,7 @@ control_pins = [7,11,13,15]
 for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
-halfstep_seq = [
+forward = [
   [1,0,0,0],
   [1,1,0,0],
   [0,1,0,0],
@@ -16,10 +16,27 @@ halfstep_seq = [
   [1,0,0,1]
 ]
 
-for i in range(10):
+reverse = [
+  [1,0,0,1],
+  [0,0,0,1],
+  [0,0,1,1],
+  [0,0,1,0],
+  [0,1,1,0],
+  [0,1,0,0],
+  [1,1,0,0],
+  [1,0,0,0]
+]
+
+for i in range(100):
   for halfstep in range(8):
     for pin in range(4):
-      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+      GPIO.output(control_pins[pin], forward[halfstep][pin])
+    time.sleep(0.001)
+    
+for i in range(100):
+  for halfstep in range(8):
+    for pin in range(4):
+      GPIO.output(control_pins[pin], reverse[halfstep][pin])
     time.sleep(0.001)
 
 GPIO.cleanup()
