1eee5f5456111252adb19fa6945a83d21e86da56
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2020-10-06 17:27:21 -0400

    making some changes

--- motor.py
+++ motor.py
@@ -1,12 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 import steps
+import sys
 
 GPIO.setmode(GPIO.BOARD)
 
 left_pins = [7,11,13,15]
 right_pins = [16,18,22,32]
 sleep = 0.001
+
+# get mode as left or right
+mode = sys.argv[1]
+
+# assume left
+control_pins = left_pins
+open_sequence = steps.getForwardSequence()
+close_sequence = steps.getBackwardSequence()
+
+if "right" == mode:
+  control_pins = right_pins
+  open_sequence = steps.getBackwardSequence()
+  close_sequence = steps.getForwardSequence()
 
 """
 64  = 45 degrees
@@ -21,28 +35,22 @@ sleep = 0.001
 rotation = ((128 - 64) / 2) + 64
 
 # initialize pins
-for pin in left_pins:
-  GPIO.setup(pin, GPIO.OUT)
-  GPIO.output(pin, 0)
-
-for pin in right_pins:
+for pin in control_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
   
-def drive(pins, step_sequence):
+def drive(step_sequence):
   for i in range(rotation):
     for step in range(len(step_sequence)):
       for pin in range(4):
-        GPIO.output(pins[pin], step_sequence[step][pin])
+        GPIO.output(control_pins[pin], step_sequence[step][pin])
       time.sleep(sleep)
 
 def open():
-  drive(left_pins, steps.getForwardSequence())
-  drive(right_pins, steps.getBackwardSequence())
+  drive(open_sequence)
 
 def close():
-  drive(left_pins, steps.getBackwardSequence())
-  drive(right_pins, steps.getForwardSequence())
+  drive(close_sequence)
 
 open()
 time.sleep(2)
