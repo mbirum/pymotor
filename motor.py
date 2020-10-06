84244f1bc989d03d2c94a10cf2c0c8c2cb3f083e
Matt Birum <mattbirum@Matts-MBP.wowway.com>
2020-10-06 17:00:51 -0400

    making some changes

--- motor.py
+++ motor.py
@@ -4,7 +4,8 @@ import steps
 
 GPIO.setmode(GPIO.BOARD)
 
-control_pins = [7,11,13,15]
+left_pins = [7,11,13,15]
+right_pins = [16,18,22,32]
 sleep = 0.001
 
 """
@@ -20,7 +21,11 @@ sleep = 0.001
 rotation = ((128 - 64) / 2) + 64
 
 # initialize pins
-for pin in control_pins:
+for pin in left_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+
+for pin in right_pins:
   GPIO.setup(pin, GPIO.OUT)
   GPIO.output(pin, 0)
   
@@ -28,7 +33,7 @@ def drive(step_sequence):
   for i in range(rotation):
     for step in range(len(step_sequence)):
       for pin in range(4):
-        GPIO.output(control_pins[pin], step_sequence[step][pin])
+        GPIO.output(right_pins[pin], step_sequence[step][pin])
       time.sleep(sleep)
 
 def open():
